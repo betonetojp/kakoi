5794b9bb4dafb873c189ad3e0ef098dcb0c7d270
betoneto <109812892+betonetojp@users.noreply.github.com>
2024-10-13 09:50:32 +0900

    v0.3.1

--- kakoi/FormSetting.Designer.cs
+++ kakoi/FormSetting.Designer.cs
@@ -206,7 +206,7 @@
             linkLabelVersion.Size = new Size(37, 15);
             linkLabelVersion.TabIndex = 10;
             linkLabelVersion.TabStop = true;
-            linkLabelVersion.Text = "v0.3.0";
+            linkLabelVersion.Text = "v0.3.1";
             linkLabelVersion.LinkClicked += LinkLabelVersion_LinkClicked;
             // 
             // checkBoxShowAvatar
