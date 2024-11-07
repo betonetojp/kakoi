5caa66b03bbc1564ffc95a206e1472d9c5193f76
betoneto <109812892+betonetojp@users.noreply.github.com>
2024-11-07 22:58:15 +0900

    バージョンをv0.4.7に更新し、更新履歴を追加
    - `FormSetting.Designer.cs`: `linkLabelVersion.Text` を `v0.4.6` から `v0.4.7` に更新
    - `kakoi.csproj`: プロジェクトのバージョンを `0.4.6` から `0.4.7` に更新
    - `readme.txt`: 2024/11/07 の更新履歴に `v0.4.7` のエントリを追加
      - ユーザーアイコンの表示処理の改善
      - `v0.4.5` で追加された `<WaitForProfile>` 設定の削除

--- kakoi/FormSetting.Designer.cs
+++ kakoi/FormSetting.Designer.cs
@@ -206,7 +206,7 @@
             linkLabelVersion.Size = new Size(37, 15);
             linkLabelVersion.TabIndex = 10;
             linkLabelVersion.TabStop = true;
-            linkLabelVersion.Text = "v0.4.6";
+            linkLabelVersion.Text = "v0.4.7";
             linkLabelVersion.LinkClicked += LinkLabelVersion_LinkClicked;
             // 
             // checkBoxGetAvatar
