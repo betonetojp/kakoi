79237f4a9eefc5180053a92afbb90df2c7eebdc0
betoneto <109812892+betonetojp@users.noreply.github.com>
2024-11-12 00:25:45 +0900

    クライアントのカラーコードを50%に変更
    オリジナルカラーの行をコメントアウトし、新しい50%カラーの行を追加しました。

--- kakoi/Tools.cs
+++ kakoi/Tools.cs
@@ -288,10 +288,12 @@ namespace kakoi
         {
             List<Client> defaultClients = [
                 // オリジナルカラー
+                //new Client { Name = "nokakoi", ColorCode = "#E4007D" },
                 //new Client { Name = "kakoi", ColorCode = "#E4007D" },
                 //new Client { Name = "lumilumi", ColorCode = "#DC7724" },
                 //new Client { Name = "noStrudel", ColorCode = "#8EB61A" },
                 // 50%カラー
+                new Client { Name = "nokakoi", ColorCode = "#F280BE" },
                 new Client { Name = "kakoi", ColorCode = "#F280BE" },
                 new Client { Name = "lumilumi", ColorCode = "#EEBB92" },
                 new Client { Name = "noStrudel", ColorCode = "#C7DB8D" },
